e2dd38313adcb7d4b61c278964698418c99bce22
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2024-08-28 09:04:12 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,27 @@
 // no javascript =)
+let entrevista_1 = {
+    entrevistador: "",
+    entravistado: "",
+    questões: {
+       p1: "Onde você nasceu, e onde vocẽ mora atualmente? ",
+       r1: "",
+       p2: "Qual era a profissão do seu sonho quando você era criança?",
+       r2: "",
+       p3: "Você trabalha? Se sim, com o que?",
+       r3: "",
+       p4: "Com que você pretende trabalhar futuramente? Pretende fazer faculdade?",
+       r4: "",
+       p5: "O que vocẽ quer alcançar na sua vida?",
+       r5: "",
+       p6: "Você pretende criar uma família?",
+       r6: "",
+       p7: "Qual sua memória preferida da escola?",
+       r7: "",
+       p8: "Você vai sentir saudades da escola? Do quê?",
+       r8: "",
+       p9: "O que você mais gostou de aprender no ensino médio?",
+       r9: "",
+       p10: "Você quer manter as amizades que fez na escola?",
+       r10: "",
+    }
+   }
